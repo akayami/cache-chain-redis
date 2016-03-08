85535f7cd4cb65fd3614df65a98a64b2b887252a
Tomasz Rakowski <tomasz.rakowski@jomediainc.com>
2016-03-08 13:39:29 -0500

    Modified to use centralized intergration tests

--- test/main.js
+++ test/main.js
@@ -1,106 +1,23 @@
 var redis = require('redis');
-var cc = require('../../cache-chain');
 
 describe('Main Tests for cache-chain-redis', function() {
 
-	var chain;
+	var di = {
+		chain: null,
+		cc: require('../../cache-chain')
+	}
 
-	beforeEach(function() {
-		chain = cc.chain({
+	beforeEach(function(done) {
+		di.chain = di.cc.chain({
 			ttl: 10000,
 			stale: 10000 * 2
 		});
 		var client = redis.createClient();
-		var layer = cc.layer(require('../index.js')(client));
-		chain.append(layer);
+		var layer = di.cc.layer(require('../index.js')(client));
+		di.chain.append(layer);
+		done();
 	});
 
-	it('It needs to set a value', function(done) {
-		var key = 'key';
-		var value = 'value';
-		chain.set(key, value, function(err, reply) {
-			if (err) {
-				done(err);
-			} else {
-				done();
-			}
-		})
-	});
+	require('../../cache-chain/test/integration')(di);
 
-	it('It needs to retrive a set value', function(done) {
-		var key = 'key';
-		var value = 'value';
-		chain.set(key, value, function(err, reply) {
-			if (err) {
-				done(err);
-			} else {
-				chain.get(key, function(err, reply) {
-					if (err) {
-						done(err);
-					} else {
-						if (reply == value) {
-							done();
-						} else {
-							done(reply)
-						}
-					}
-				})
-			}
-		})
-	});
-
-	it('It needs to delete a set value', function(done) {
-		var key = 'key';
-		var value = 'value';
-		chain.set(key, value, function(err, reply) {
-			if (err) {
-				done(err);
-			} else {
-				chain.delete(key, function(err) {
-					if (err) {
-						cb(err);
-					} else {
-						chain.get(key, function(err, reply) {
-							if (err) {
-								if(err instanceof cc.error.notFound) {
-									done();
-								} else {
-									done('Wrong error message returned');
-								}
-							} else {
-								done('Expected error message. Got correct reply instead')
-							}
-						})
-					}
-				})
-			}
-		})
-	});
-
-	it('A value needs to timeout', function(done) {
-		var key = 'key';
-		var value = 'value';
-		chain.set(key, value, {
-			ttl: 10,
-			stale: 5
-		}, function(err, reply) {
-			if (err) {
-				done(err);
-			} else {
-				setTimeout(function() {
-					chain.get(key, function(err, reply) {
-						if (err) {
-							if(err instanceof cc.error.notFound) {
-								done();
-							} else {
-								done('Wrong error message returned');
-							}
-						} else {
-							done('Expected error message. Got correct reply instead')
-						}
-					})
-				}, 15);
-			}
-		})
-	});
 });
